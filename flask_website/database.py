b19b885f0c341c6ca422d083b2651e03ae915e1b
jinxin <jinxin@qfpay.com>
2015-04-28 22:21:08 +0800

    fixs

--- flask_website/database.py
+++ flask_website/database.py
@@ -4,7 +4,8 @@ from sqlalchemy import create_engine, Column, Integer, String, DateTime, \
 from sqlalchemy.orm import scoped_session, sessionmaker, backref, relation
 from sqlalchemy.ext.declarative import declarative_base
 
-from werkzeug import cached_property, http_date
+from werkzeug.utils import cached_property
+from werkzeug.http import http_date
 
 from flask import url_for, Markup
 from flask_website import app, search
